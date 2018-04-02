58936bf85d3b7e508429d88c3712990d36b2ec4a
Susumu Miwa <miwarin@gmail.com>
2018-04-02 19:41:12 +0900

    add
    組み込みLinuxデバイスドライバの作り方 (1) - Qiita
    https://qiita.com/take-iwiw/items/1fdd2e0faaaa868a2db2

---
+++ 01/test.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>
+
+static int test_init(void)
+{
+	printk("Hello my module\n");
+	return 0;
+}
+
+static void test_exit(void)
+{
+	printk("bye bye my module\n");
+}
+
+module_init(test_init);
+module_exit(test_exit);
+
+
